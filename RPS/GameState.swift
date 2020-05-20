bfdd10a67bd4bcd1da886104c578a0b890f88b67
Charlie Wallin <charliekwallin@gmail.com>
2020-05-20 16:06:04 -0400

    RPS finished

---
+++ RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Charles Wallin on 5/19/20.
+//  Copyright © 2020 Charles Wallin. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
